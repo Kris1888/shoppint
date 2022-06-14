8b31e0ac814ceeff7e0457d908aa0106bc771623
DESKTOP-5JA86CS\Administrator <2356245162@qq.com>
2022-06-14 13:56:47 +0800

    添加模型

--- dao/src/main/java/com/shopping/dao/entity/Voucher.java
+++ dao/src/main/java/com/shopping/dao/entity/Voucher.java
@@ -1,8 +1,11 @@
 package com.shopping.dao.entity;
 
 import lombok.Data;
+
 import javax.persistence.Table;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  * @author Kris_Y
@@ -13,4 +16,73 @@ import java.io.Serializable;
 @Table(name = "voucher")
 public class Voucher implements Serializable {
 
+    /**
+     * 店铺优惠券id
+     */
+    private Integer id;
+
+    /**
+     * 店铺id
+     */
+    private Integer shopId;
+
+    /**
+     * 优惠券名称
+     */
+    private String title;
+
+    /**
+     * 满多少钱
+     */
+    private BigDecimal fullMoney;
+
+    /**
+     * 减多少钱
+     */
+    private BigDecimal amount;
+
+    /**
+     * 开始时间
+     */
+    private LocalDateTime startTime;
+
+    /**
+     * 结束时间
+     */
+    private LocalDateTime endTime;
+
+    /**
+     * 所需积分
+     */
+    private Integer point;
+
+    /**
+     * 发行数量
+     */
+    private Integer count;
+
+    /**
+     * 领取数量
+     */
+    private Integer receiveNum;
+
+    /**
+     * 添加时间
+     */
+    private LocalDateTime addtime;
+
+    /**
+     * 优惠券类型
+     */
+    private Integer type;
+
+    /**
+     * 删除状态
+     */
+    private Integer del;
+
+    /**
+     * 产品 ID
+     */
+    private Integer proid;
 }
